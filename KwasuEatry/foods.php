d9614f1d4c902ebf7a74b2afc72df9aba101b80b
unicodebeast <unicodebeast@gmail.com>
2019-04-10 18:21:05 +0000

    updated features

---
+++ KwasuEatry/foods.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Foods</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div class="container">
+<header class="header">
+            <div class="logo"><img class="logoimg" width="130px" height="70px" src="Other Files/logo.png" alt="">
+            <h4>KwasuDelight</h4>
+            </div>
+            <div class="nav-items">
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SERVICES</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                </ul>
+            </div>
+        </header>
+        <section class="section">
+        <div class="titles">
+                <h1 font-face="simplicity">KwasuDelight Foods</h1>
+                <h3>What would you like to eat</h3>
+            </div>
+            <div class="background">
+                <img class="main-drink" width="70%" src="IMAGES/r2.png" alt="drink">
+            </div>
+            <div class="background">
+                <div class="Varieties">
+                        <a href="foods.php"><img src="IMAGES/i6.png" alt="food" class="images"></a>
+                        <a href="barbique.php"><img src="IMAGES/i3.png" alt="food" class="images"></a>
+                        <a href="drinks.php"><img src="IMAGES/i9.png" alt="food" class="images"></a>
+                        <a href="pasteries.php"><img src="IMAGES/i12.png" alt="food" class="images"></a>
+                </div>
+                </div>
+            </div>            
+        </section>
+    <footer class="footer"> &COPY; KWASU GROUP 17</footer>
+</div>
+</body>
+</html>

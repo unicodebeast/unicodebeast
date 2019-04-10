ec0bf3214a8594526b24d7f684f246a2c268d026
unicodebeast <unicodebeast@gmail.com>
2019-04-10 17:14:21 +0000

    KWASU GROUP 17 PROJECT "Eatery Website"

---
+++ KwasuEatry/drinks.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<div class="container">
+<header class="header">
+            <div class="logo"><img class="logoimg" width="130px" height="70px" src="Other Files/logo.png" alt="">
+            <h4>KwasuDelight</h4>
+            </div>
+            <div class="nav-items">
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SERVICES</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                </ul>
+            </div>
+        </header>
+        <section class="section">
+            <div class="titles">
+                <h1 font-face="simplicity">KwasuDelight Drinks</h1>
+                <h3>Choose your prefered Drinks</h3>
+            </div>
+            <div class="background">
+                <img class="main-drink" src="IMAGES/i11.png" alt="drink">
+            </div>
+        </section>
+    <footer class="footer"> &COPY; KWASU GROUP 17</footer>
+</div>
+</body>
+</html>
---
+++ KwasuEatry/index.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KWASU EATERY</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <div class="logo"><img class="logoimg" width="130px" height="70px" src="Other Files/logo.png" alt="">
+            <h4>KwasuDelight</h4>
+            </div>
+            <div class="nav-items">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">SERVICES</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                </ul>
+            </div>
+        </header>
+        <section class="section">
+            <div class="background">
+                <img width="407px" height="154px" src="Other Files/logo1.png" alt="" class="compname">
+                <img class="image" src="IMAGES/top.png" alt="FoodImg">
+                <div class="Varieties">
+                        <img src="IMAGES/i6.png" alt="food" class="images">
+                        <img src="IMAGES/i3.png" alt="food" class="images">
+                        <a href="drinks.php"><img src="IMAGES/i9.png" alt="food" class="images"></a>
+                        <img src="IMAGES/i12.png" alt="food" class="images">
+                </div>
+            </div>            
+        </section>
+        <footer class="footer"> &COPY; KWASU GROUP 17</footer>
+    </div>
+</body>
+</html>
+
+
+<!-- JAVASCRIPT CODES -->

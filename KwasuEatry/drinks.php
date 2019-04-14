7a8f193bf5dd1bcb1682aa8a2a8f8eed3fdc2028
unicodebeast <unicodebeast@gmail.com>
2019-04-14 22:25:12 +0000

    modified pages

--- KwasuEatry/drinks.php
+++ KwasuEatry/drinks.php
@@ -9,20 +9,29 @@
 <body>
 <div class="container">
 <?php
-session_start();
 
-$checkedDrinks = $_SESSION['checked'];
-$drinkQty = $_SESSION['qty'];
+if (isset($_POST['checked'])) {
+    foreach ($_POST['checked'] as $drink => $key) {
+        echo $_POST[$key].'<br>';
+    }
+}
+$checkedDrinks = $_POST['checked'];
+$drinkQty = $_POST['qty'];
+
 function getItems($item, $qty)
 {
-    $result = array();
+    $result;
     for ($i = 0; $i < count($checkedDrinks); ++$i) {
-        array_push($result, 'you requested '.$qty[$i].' Plastics of '.$item[$i].'<br>');
+        $result.'<h3> you requested '.$qty[$i].' Plastics of '.$item[$i].'</h3> <br>';
     }
 
     return $result;
 }
-$purchedItem = getItems($checkedDrinks, $drinkQty);
+// if (isset($checkedDrinks) && isset($drinkQty)) {
+//     foreach ($checkedDrinks as $n) {
+//         echo $n;
+//     }
+// }
 ?>
         <?php
         require_once 'header.php';
@@ -51,63 +60,63 @@ $purchedItem = getItems($checkedDrinks, $drinkQty);
 
                             <tr class="t-row">
                                     <td class="item">COCACOLA</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="COCACOLA" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked[cocacola]" value="COCACOLA" class="checkBox">    </td>
                                     <td class="price"> 100 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['cocacola']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">PEPSI</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="PEPSI" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked[pepsi]" value="PEPSI" class="checkBox">    </td>
                                     <td class="price"> 100 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['pepsi']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">MIRINDA</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="MIRINDA" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked['mirinda']" value="MIRINDA" class="checkBox">    </td>
                                     <td class="price"> 100 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['mirinda']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">BIGI APPLE</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="BIGI APPLE" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked['big_apple']" value="BIGI APPLE" class="checkBox">    </td>
                                     <td class="price"> 150 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['big_apple']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">BIGI COLA</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="BIGI COLA" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked['bigi_cola']" value="BIGI COLA" class="checkBox">    </td>
                                     <td class="price"> 150 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['bigi_cola']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">SPRITE</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="SPRITE" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked['sprite']" value="SPRITE" class="checkBox">    </td>
                                     <td class="price"> 120 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['sprite']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">CWAY NUTRI MILK</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="CWAY NUTRI MILK" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked['cway']" value="CWAY NUTRI MILK" class="checkBox">    </td>
                                     <td class="price"> 150 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['cway']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">SMOOVE</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="SMOOVE" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked['smoove']" value="SMOOVE" class="checkBox">    </td>
                                     <td class="price"> 100 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['smoove']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">NUTRI YO</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="NUTRI YO" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked['nutri_yo']" value="NUTRI YO" class="checkBox">    </td>
                                     <td class="price"> 200 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['nutri_yo']" id=""></td>
                             </tr>
                             <tr class="t-row">
                                     <td class="item">LACASERA</td>
-                                    <td class="checkbox"><input type="checkbox" name="checked[]" value="LACASERA" class="checkBox">    </td>
+                                    <td class="checkbox"><input type="checkbox" name="checked['lacasera']" value="LACASERA" class="checkBox">    </td>
                                     <td class="price"> 100 </td>   
-                                    <td class="quantity"><input type="number" name="qty[]" id=""></td>
+                                    <td class="quantity"><input type="number" name="qty['lacasera']" id=""></td>
                             </tr>
 
                         </table>
--- KwasuEatry/index.php
+++ KwasuEatry/index.php
@@ -3,29 +3,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>KWASU EATERY</title>
-  </head>
+    <title>Document</title>
+    <title>login</title>
+    <link rel="stylesheet" href="main.css">
+</head>
 <body>
     <div class="container">
-       <?php
-       require_once 'header.php';
-       ?>
-        <section class="section">
-            <div class="background">
-                <img width="407px" height="154px" src="Other Files/logo1.png" alt="" class="compname">
-                <img class="image" src="IMAGES/top.png" alt="FoodImg">
-                <div class="Varieties">
-                        <a href="foods.php"><img src="IMAGES/i6.png" alt="food" class="images"></a>
-                        <a href="barbique.php"><img src="IMAGES/i3.png" alt="food" class="images"></a>
-                        <a href="drinks.php"><img src="IMAGES/i9.png" alt="food" class="images"></a>
-                        <a href="pasteries.php"><img src="IMAGES/i12.png" alt="food" class="images"></a>
+        <div class="imgs">
+            <img class="l-1" width="15%"src="Other Files/logo.png" alt="">
+            <img class="l-2" width="30%"src="Other Files/logo1.png" alt="">
+        </div> 
+        <div class="block">`
+            <div class="land-box">
+                <div class="bgland1">
+                    <div class="bgland">
+                        <img src="IMAGES/img6.png" alt="disp">  
+                    </div>
+                    <div class="login">
+                        <div class="log-info">
+                            <form action="home.php" method="post">
+                                FULL NAME:<input type="text" name="Full Name" id="" required>
+                                EMAIL:<input type="email" name="Email" id="" required>
+                                MATRIC NO:<input type="text" name="Matric Number" id="" required>
+                                PASSWORD:<input type="password" name="Password" id="" required>
+                                <input type="submit" value="Login">
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>            
-        </section>
-      <?php
-      require_once 'footer.php';
-      ?>
+                </div>        
+        </div>
+       <div class="logfooter">
+           &copy;KWASU GROUP 17 EATRY WEBPAGE
+       </div>
     </div>
+   
 </body>
 </html>
